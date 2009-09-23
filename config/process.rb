9fb6c251450adfb3911a0d6ce601b841d4325a9f
Jeff Rafter <jeff@baobabhealth.org>
2009-09-22 21:59:37 -0700

    Update with latest fixes from server config

--- config/process.rb
+++ config/process.rb
@@ -1,4 +1,8 @@
-loop do
-  Lokii::Processor.process
-  sleep(Lokii::Config.interval)  
-end
+Lokii::Processor.servers.each{|server| server.connect }
+
+ loop do
+   Lokii::Processor.process
+   sleep(Lokii::Config.interval)  
+ end
+
+Lokii::Processor.servers.each{|server| server.disconnect }
